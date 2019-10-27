52233452859731ab013dc8aa80174bec3862b157
jason71319jason <jason71319jason@gmail.com>
2019-10-27 22:06:51 +0800

    Update shell script

---
+++ exec.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker start p4_tutorial 
+docker attach p4_tutorial
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 set -e
 set -x
 
-sudo docker run -it --privileged -e DISPLAY              -v /tmp/.X11-unix:/tmp/.X11-unix              -v /lib/modules:/lib/modules              p4lang/tutorials
+xhost +si:localuser:root
+sudo docker run --name p4_tutorial -it --privileged -e DISPLAY              -v /tmp/.X11-unix:/tmp/.X11-unix              -v /lib/modules:/lib/modules              p4lang/tutorials
