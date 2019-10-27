b38dd683017c99217dcdd3b5530a9f5c211cf5af
jason71319jason <jason71319jason@gmail.com>
2019-10-27 22:34:32 +0800

    Update shell script

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-set -e
-set -x
-
-xhost +si:localuser:root
+#!/bin/sh
+sudo host +si:localuser:root
 sudo docker run --name p4_tutorial -it --privileged -e DISPLAY              -v /tmp/.X11-unix:/tmp/.X11-unix              -v /lib/modules:/lib/modules              p4lang/tutorials
